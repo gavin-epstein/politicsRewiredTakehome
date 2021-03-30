28344dc72088ce89bf523327ed5180b2c965f986
gavin-epstein <wreakjoy@gmail.com>
2021-03-30 15:28:29 +0000

    Cleanup and optimization

--- migrations/current/02-prevent-sends-to-triple-unsubscribes.sql
+++ migrations/current/02-prevent-sends-to-triple-unsubscribes.sql
@@ -1,29 +1,10 @@
--- create or replace function fake_test_pass() returns trigger as $$
--- declare
--- begin
---   if NEW.body = 'fake pass body' then
---     raise 'Cannot send message - frequently unsubscribed recipient';
---   end if;
-
---   return NEW;
--- end;
--- $$ language plpgsql;
-
--- create trigger fake_test_pass
---   before insert
---   on outbound_messages
---   for each row
---   execute procedure fake_test_pass();
-
 --Table for storing which phone has unsubscribed from which profile
---Automatically indexed by phone_number, no need to explicitly declare an index to 
---get better performance
+--Automatically indexed by phone_number, no need to explicitly declare an index to get better performance
 create table unsubscribed_phone_numbers (
     phone_number text unique primary key,
     unsubscribed_profiles uuid[] default '{}'
 );
---if the table delivery_reports had already been committed and had data, we would need 
---a step to populate unsubscribed_phone_numbers but I'm leaving that out for this assignment
+--if the table delivery_reports had already been committed and had data, we would need a step to populate unsubscribed_phone_numbers but I'm leaving that out for this assignment since delivery_reports is in current 
 
 --Trigger for adding to unsubscribed
 create or replace function unsubscribe_by_phone() 
@@ -35,15 +16,13 @@ as $$
         profiles_array unsubscribed_phone_numbers.unsubscribed_profiles%type;
         loop_id uuid;
     begin
-        select to_number, profile_id 
-            into new_phone_number, new_profile_id
-            from outbound_messages 
+        select a.to_number, a.profile_id, b.unsubscribed_profiles
+            into new_phone_number, new_profile_id, profiles_array
+            from outbound_messages as a
+            left join unsubscribed_phone_numbers as b
+            on b.phone_number = a.to_number
             where id = NEW.message_id;
-        select unsubscribed_profiles 
-            into profiles_array 
-            from unsubscribed_phone_numbers
-            where phone_number = new_phone_number;
-        if not found then
+        if profiles_array is null then
             --if this phone has never unsubscribed
             insert into unsubscribed_phone_numbers 
             values (new_phone_number,array[new_profile_id]);
